14f9b1ccb750e7316507560cb66c2216425d16db
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-03-07 23:28:39 +0000

    update

--- script.py
+++ script.py
@@ -20,6 +20,11 @@ for _ in range(2):
 
 def calculate_score(cards):
 
-    if sum(cards)
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
 
     return sum(cards)
